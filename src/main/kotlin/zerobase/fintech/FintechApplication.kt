ecdbd13ea0a40440f6f5dcd29a92b87e68313abd
WonJin <try3982@kakao.com>
2025-02-10 19:39:24 +0900

    작업환경 구성

---
+++ src/main/kotlin/zerobase/fintech/FintechApplication.kt
@@ -0,0 +1,11 @@
+package zerobase.fintech
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FintechApplication
+
+fun main(args: Array<String>) {
+    runApplication<FintechApplication>(*args)
+}
---
+++ src/test/kotlin/zerobase/fintech/FintechApplicationTests.kt
@@ -0,0 +1,13 @@
+package zerobase.fintech
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FintechApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
