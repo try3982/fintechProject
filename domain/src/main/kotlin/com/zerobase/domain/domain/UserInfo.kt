13b2add8a428defa570618cbbe2f81795f5821f5
WonJin <try3982@kakao.com>
2025-02-13 23:32:00 +0900

    UserInfo 클래스 추가

--- domain/src/main/kotlin/com/zerobase/domain/domain/UserInfo.kt
+++ domain/src/main/kotlin/com/zerobase/domain/domain/UserInfo.kt
@@ -1,6 +1,24 @@
-package com.zerobase.domain.domain;
+package com.zerobase.domain.domain
 
-public class UserInfo {
+import javax.persistence.*
 
+@Entity
+@Table(name = "USER_INFO")
+class UserInfo(
+    @Column(name = "user_key")
+    val userKey: String,
 
+    @Column(name = "user_reg_num")
+    val userRegisterationNumber: String,
+
+    @Column(name = "user_nm")
+    val userName: String,
+
+    @Column(name = "user_icm_amt")
+    val userIncomeAmount: Long
+) {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    val id: Long? = null
 }
