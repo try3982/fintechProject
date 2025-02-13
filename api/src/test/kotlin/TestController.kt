d5d76cc62beffec53d94ab46be7c412a78b20386
WonJin <try3982@kakao.com>
2025-02-13 23:59:08 +0900

    Test Controller 테스트용 추가

--- api/src/test/kotlin/TestController.kt
+++ api/src/test/kotlin/TestController.kt
@@ -1,8 +1,10 @@
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/test")
+@RequestMapping("fintech/api/v1")
 
 class TestController {
+    @GetMapping("/test/get")
 }
