cc458ca803d76906c98d196bbd338c18cf8f58d8
WonJin <try3982@kakao.com>
2025-02-13 23:39:27 +0900

    UserInfoRepository 클래스 추가

---
+++ domain/src/main/kotlin/com/zerobase/domain/repository/UserInfoRepository.kt
@@ -0,0 +1,9 @@
+package com.zerobase.domain.repository
+
+import com.zerobase.domain.domain.UserInfo
+import org.springframework.boot.autoconfigure.security.SecurityProperties
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface UserInfoRepository : JpaRepository<UserInfo, Long> {
+
+}
