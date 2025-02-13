ada071175548c16c6161df6e89274f200940ce14
WonJin <try3982@kakao.com>
2025-02-14 00:16:02 +0900

    userKey 넘겨주기

--- api/src/test/kotlin/TestService.kt
+++ api/src/test/kotlin/TestService.kt
@@ -4,11 +4,10 @@ import org.springframework.stereotype.Service
 import kotlin.reflect.jvm.internal.impl.descriptors.CallableDescriptor.UserDataKey
 
 @Service
-
 class TestService (
     private val userInfoRepository : UserInfoRepository
 ) {
-    fun testGetService(userKey: String) = userInfoRepository.findByUserKey(userKey)
+    fun testGetService(userKey: String) = userInfoRepository.findByUserKey(userKey).toDto()
     fun UserInfo.todto() = TestDto.UserInfoDto(userKey, userRegisterationNumber, userName, userIncomeAmount)
 
 }
